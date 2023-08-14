09bfefab3cf920ed5878680b7385c4c6ef5cb413
raresb2001 <raresb2001@gmail.com>
2023-08-14 11:07:05 +0300

    Update

--- InvitatieNunta/settings.py
+++ InvitatieNunta/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-z6y)%6-l$st#@n9r2*2f#pbl)g8aq(=ao#3t6#u1tbjwfjxc2%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['raresb2001.pythonanywhere.com']
 
 
 # Application definition
